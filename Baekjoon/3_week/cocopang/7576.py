012d7db1504bf355d6cfb9cbcd163635fbb5c8af
c0c0pang <coc1789@naver.com>
2023-03-04 14:33:04 +0900

    style: 함수명 오타 수정
    in_ragne -> in_range

--- Baekjoon/3_week/cocopang/7576.py
+++ Baekjoon/3_week/cocopang/7576.py
@@ -1,33 +1,36 @@
 import sys
 from collections import deque
-n,m = map(int,sys.stdin.readline().rstrip().split())
+n, m = map(int, sys.stdin.readline().rstrip().split())
 
 array = [
-    list(map(int,sys.stdin.readline().rstrip().split()))
+    list(map(int, sys.stdin.readline().rstrip().split()))
     for _ in range(m)
 ]
-dx,dy=[-1,1,0,0],[0,0,-1,1]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 box = deque([])
-def in_ragne(nx,ny):
-    return nx < 0 or nx >=m or ny<0 or ny>=n
+
+
+def in_range(nx, ny):
+    return nx < 0 or nx >= m or ny < 0 or ny >= n
+
+
 def bfs():
     for i in range(m):
         for j in range(n):
             if array[i][j] == 1:
-                box.append([i,j,0])
+                box.append([i, j, 0])
     while len(box):
         global vd
-        vx,vy,vd = box.popleft()
-        for x,y in zip(dx,dy):
-            nx,ny = x+vx,y+vy
-            if in_ragne(nx,ny):
+        vx, vy, vd = box.popleft()
+        for x, y in zip(dx, dy):
+            nx, ny = x+vx, y+vy
+            if in_range(nx, ny):
                 continue
-            if array[nx][ny] !=0:
+            if array[nx][ny] != 0:
                 continue
-            array[nx][ny]=1
-            box.append([nx,ny,vd+1])
-            
-        
+            array[nx][ny] = 1
+            box.append([nx, ny, vd+1])
+
 
 bfs()
 check = True
@@ -35,7 +38,7 @@ for i in range(m):
     if not check:
         break
     for j in range(n):
-        if array[i][j]==0:
+        if array[i][j] == 0:
             check = False
 if check:
     print(vd)
